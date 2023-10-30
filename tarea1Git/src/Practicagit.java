5b6ac107a25c04c8c5012b1ace6c7685d066e34d
Ernesto Ruiz Fernandez <a_ernesto.ruiz.fernandez@iespablopicasso.es>
2023-10-30 09:12:05 +0100

    Añado un metodo espar en el proyecto

--- tarea1Git/src/Practicagit.java
+++ tarea1Git/src/Practicagit.java
@@ -4,6 +4,8 @@ public class Practicagit {
 	public static void main(String[] args) {
 		int numero1 = 0;
 		int numero2 = 0;
+		int resultado = 0;
+		
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -15,8 +17,16 @@ public class Practicagit {
 		
 		numero2 = Integer.parseInt(sc.nextLine());
 		
+		resultado = numero1 + numero2;
 		
-		System.out.println("La suma de los dos numeros es: " + (numero1 + numero2));
+		System.out.println("La suma de los dos numeros es: " + resultado);
+		
+		if (resultado % 2 == 0) {
+			System.out.println("El numero es par");
+		}
+		else {
+			System.out.println("El numero no es par");
+		}
 		
 		sc.close();
 	}
